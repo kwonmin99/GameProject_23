2905abdaef966246499cb54838eb5e71ca10d899
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2023-05-19 16:34:56 +0900

    Bullet 보완

--- BinaryHeroes/Assets/Scripts/Bullet.cs
+++ BinaryHeroes/Assets/Scripts/Bullet.cs
@@ -20,35 +20,25 @@ public class Bullet : MonoBehaviour
         this.damage = damage;
         this.per = per;
 
-        if (per > -1){
+        if (per >= 0){
             rigid.velocity = dir * 15f; // 속력 곱해서 총알 날아가는 속도 증가
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
-        if(!collision.CompareTag("Enemy") || per == -1) //-100
+        if(!collision.CompareTag("Enemy") || per == -100) //-100
             return;
 
         per--; // 관통력 줄여줌
 
-        if(per == -1){
+        if(per < 0){
             rigid.velocity = Vector2.zero;
             gameObject.SetActive(false);
         }
     }
 
-    
-    void OnTriggerExit2D(Collider2D collision) {
-        if(!collision.CompareTag("Area") || per == -1) // -100
-            return;
-        
-        per--;
-
-        // if (per == -1){
-        //     rigid.velocity = Vector2.zero;
-        //     gameObject.SetActive(false);
-        // }
-        
+    //area 밖에 나가면 투사체 제거
+    void OnTriggerExit2D(Collider2D collision) {        
         if(!collision.CompareTag("Area") || per == -100)
             return;
         
